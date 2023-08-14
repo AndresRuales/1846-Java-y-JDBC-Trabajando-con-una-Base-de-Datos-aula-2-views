9b2d3c41967133f42a2a7b8ce7d516d87c5ca0d1
Andres Ruales <danielandresruales@hotmail.com>
2023-08-13 19:21:04 -0500

    Se empieza el curso de JDBC con java, se descarga plantilla con el fronted, se crea una DB en MySQL, que registra los productos

---
+++ src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -0,0 +1,40 @@
+package com.alura.jdbc.controller;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoController {
+
+	public void modificar(String nombre, String descripcion, Integer id) {
+		// TODO
+	}
+
+	public void eliminar(Integer id) {
+		// TODO
+	}
+
+	public List<?> listar() throws SQLException{
+		Connection con = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/control_de_stock?useTimeZone=true&serverTimeZone=UTC", 
+				"root", "root");
+		
+		Statement statement = (Statement) con.createStatement();
+		
+		boolean result = statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+		
+		System.out.println(result);
+		con.close();
+		
+		return new ArrayList<>();
+	}
+
+    public void guardar(Object producto) {
+		// TODO
+	}
+
+}
